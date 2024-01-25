f6b8801061c0c257d9216094676f937a41e88589
Erthium <ertyum35@gmail.com>
2024-01-25 06:31:02 +0300

    Fix issues in get request

--- src/poke/poke.controller.ts
+++ src/poke/poke.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PokeService } from './poke.service';
 
 @Controller('poke')
@@ -11,27 +11,48 @@ export class PokeController {
         return this.pokeService.getRandomPokemon();
     }
 
-    // get pokemon by line number
+    // get pokemon by index
     @Get(':index')
-    async getPokemonByIndex(index: number): Promise<string> {
-        return this.pokeService.getPokemonName(index);
+    async getPokemonByIndex(@Param() params: any): Promise<string> {
+        //parse params.index to number
+        try{
+            const index: number = parseInt(params.index);
+            return this.pokeService.getPokemonName(index);
+        }
+        catch (error){
+            console.log(error);
+            return '';
+        }
     }
 
     @Get('all/:index')
-    async getAllPokemonAbilities(index: number): Promise<string[]> {
-        const pokeName: string = await this.pokeService.getPokemonName(index)
-        return this.pokeService.getAllPokemonAbilities(pokeName);
+    async getAllPokemonAbilities(@Param() params: any): Promise<string[]> {
+        try{
+            const index: number = parseInt(params.index);
+            const pokeName: string = await this.pokeService.getPokemonName(index)
+            return this.pokeService.getAllPokemonAbilities(pokeName);
+        }
+        catch (error){
+            console.log(error);
+            return [];
+        }
     }
 
     @Get('def/:index')
-    async getAllPokemonAbilitiesDef(index: number): Promise<string[]> {
-        const pokeName: string = await this.pokeService.getPokemonName(index)
-        const ability_info: string[] = [];
-        const allPokeAbilities: string[] = await this.pokeService.getAllPokemonAbilities(pokeName);
-        for (let i = 0; i < allPokeAbilities.length; i++) {
-            ability_info.push(await this.pokeService.getAbilityInfo(allPokeAbilities[i]));
+    async getAllPokemonAbilitiesDef(@Param() params: any): Promise<string[]> {
+        try{
+            const index: number = parseInt(params.index);
+            const pokeName: string = await this.pokeService.getPokemonName(index)
+            const ability_info: string[] = [];
+            const allPokeAbilities: string[] = await this.pokeService.getAllPokemonAbilities(pokeName);
+            for (let i = 0; i < allPokeAbilities.length; i++) {
+                ability_info.push(await this.pokeService.getAbilityInfo(allPokeAbilities[i]));
+            }
+            return ability_info;
         }
-        return ability_info;
+        catch (error){
+            console.log(error);
+            return [];
+        }
     }
-
 }
--- src/poke/poke.service.ts
+++ src/poke/poke.service.ts
@@ -12,18 +12,18 @@ export class PokeService {
         return randomLine;
     }
 
-    
+
     // get the pokemon name by line number
     async getPokemonName(index: number): Promise<string> {
         const pokemonInfo = await CsvService.getLineByIndex(AppConfig.poke_data_path, index);
-        return pokemonInfo[0];
+        return pokemonInfo.split(',')[0];
     }
 
 
     async getAllPokemonAbilities(name: string): Promise<string[]> {
         const abilityList: string[] = [];
-        const ability_names: string[] = (await CsvService.getLineByIndex(AppConfig.poke_ability_data_path, 0)).split(',').slice(1);
-        const pokemon_abilities: string[] = (await CsvService.getLineByColumn(AppConfig.poke_ability_data_path, 0, name)).split(',').slice(1);
+        const ability_names: string[] = (await CsvService.getLineByIndex(AppConfig.poke_ability_data_path, 1)).split(',');
+        const pokemon_abilities: string[] = (await CsvService.getLineByColumn(AppConfig.poke_ability_data_path, 0, name)).split(',');
         for (let i = 0; i < pokemon_abilities.length; i++) {
             if (pokemon_abilities[i] === '1') {
                 abilityList.push(ability_names[i]);
