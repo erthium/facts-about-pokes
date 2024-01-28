49676837431a4bcb5b50d08609cafc6cd5aedfc2
Erthium <ertyum35@gmail.com>
2024-01-28 04:28:14 +0300

    Add case sensitivity option to CSV service

--- src/csv/csv.service.ts
+++ src/csv/csv.service.ts
@@ -5,91 +5,93 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CsvService {
 
-  static separator: string = ',';
+    static separator: string = ',';
 
-  
-  static async getNumberOfLines(csvFilePath: string): Promise<number> {
-    return new Promise((resolve, reject) => {
-      const stream = fs.createReadStream(csvFilePath);
-      const reader = readline.createInterface({
-        input: stream,
-        crlfDelay: Infinity,
-      });
-
-      let lineCount = 0;
-
-      reader.on('line', () => {
-        lineCount++;
-      });
-
-      reader.on('close', () => {
-        resolve(lineCount);
-      });
-
-      reader.on('error', (error) => {
-        reject(error);
-      });
-    });
-  }
-
-
-  static async getLineByIndex(csvFilePath: string, lineNumber: number): Promise<string> {
-    return new Promise((resolve, reject) => {
+    
+    static async getNumberOfLines(csvFilePath: string): Promise<number> {
+      return new Promise((resolve, reject) => {
         const stream = fs.createReadStream(csvFilePath);
         const reader = readline.createInterface({
           input: stream,
           crlfDelay: Infinity,
         });
-  
-        let currentLine = 0;
-  
-        reader.on('line', (line) => {
-          currentLine++;
-          if (currentLine === lineNumber) {
-            const content = line;
-            resolve(content);
-          }
+
+        let lineCount = 0;
+
+        reader.on('line', () => {
+          lineCount++;
         });
-  
+
         reader.on('close', () => {
-          reject(new Error('End of file reached before finding the line.'));
+          resolve(lineCount);
         });
-  
+
         reader.on('error', (error) => {
           reject(error);
         });
       });
-  }
+    }
 
 
-  static async getLineByColumn(csvFilePath: string, columnIndex: number, columnData: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        const stream = fs.createReadStream(csvFilePath);
-        const reader = readline.createInterface({
-          input: stream,
-          crlfDelay: Infinity,
+    static async getLineByIndex(csvFilePath: string, lineNumber: number): Promise<string> {
+      return new Promise((resolve, reject) => {
+          const stream = fs.createReadStream(csvFilePath);
+          const reader = readline.createInterface({
+            input: stream,
+            crlfDelay: Infinity,
+          });
+    
+          let currentLine = 0;
+    
+          reader.on('line', (line) => {
+            currentLine++;
+            if (currentLine === lineNumber) {
+              const content = line;
+              resolve(content);
+            }
+          });
+    
+          reader.on('close', () => {
+            reject(new Error('End of file reached before finding the line.'));
+          });
+    
+          reader.on('error', (error) => {
+            reject(error);
+          });
         });
-  
-        let currentLine = 0;
-  
-        reader.on('line', (line) => {
-          currentLine++;
-          const columns = line.split(this.separator);
-          if (columns[columnIndex] === columnData) {
-            const content = line;
-            resolve(content);
-          }
-        });
-  
-        reader.on('close', () => {
-          reject(new Error('End of file reached before finding the column.'));
-        });
-  
-        reader.on('error', (error) => {
-          reject(error);
-        });
+    }
+
+
+    static async getLineByColumn(csvFilePath: string, columnIndex: number, columnData: string, caseSensitive: boolean = false): Promise<string> {
+      return new Promise((resolve, reject) => {
+          if (!caseSensitive) columnData = columnData.toLowerCase();
+          const stream = fs.createReadStream(csvFilePath);
+          const reader = readline.createInterface({
+              input: stream,
+              crlfDelay: Infinity,
+          });
+
+          let currentLine = 0;
+
+          reader.on('line', (line) => {
+              currentLine++;
+              const columns = line.split(this.separator);
+              if (!caseSensitive) columns[columnIndex] = columns[columnIndex].toLowerCase();
+              if (columns[columnIndex] === columnData) {
+                  const content = line;
+                  resolve(content);
+              }
+          });
+
+          reader.on('close', () => {
+              reject(new Error('End of file reached before finding the column.'));
+          });
+
+          reader.on('error', (error) => {
+              reject(error);
+          });
       });
-  }
+    }
 
 
   static async getColumnByIndex(csvFilePath: string, columnIndex: number): Promise<string[]> {
